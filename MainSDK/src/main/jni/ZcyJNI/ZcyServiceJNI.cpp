54a2bfc17ab1b573195b8ca8e8a095fd9b8d8566
zuochengyao <zuochengyao@megvii.com>
2018-02-05 18:23:10 +0800

    no message

--- MainSDK/src/main/jni/ZcyJNI/ZcyServiceJNI.cpp
+++ MainSDK/src/main/jni/ZcyJNI/ZcyServiceJNI.cpp
@@ -19,40 +19,42 @@ static JavaVM *mJVM = NULL;
 static const char *classPathName = JNI_PACKAGE_NAME"engine/JniNative";
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-JNIEXPORT void JNICALL zcy_native_helloWorld(JNIEnv, jclass) {
+JNIEXPORT void JNICALL zcy_native_helloWorld(JNIEnv, jclass)
+{
     LOGI("This just a test for Android Studio NDK JNI developer!");
 }
 
-JNIEXPORT jint JNICALL zcy_native_add(JNIEnv, jclass, jint a, jint b) {
+JNIEXPORT jint JNICALL zcy_native_add(JNIEnv, jclass, jint a, jint b)
+{
     return a + b;
 }
 
 JNIEXPORT jstring JNICALL sayHello(JNIEnv *env, jobject)
 {
     LOGI("sayHello");
-    const char* hello = "Hello from jni";
-    return env -> NewStringUTF(hello);
+    const char *hello = "Hello from jni";
+    return env->NewStringUTF(hello);
 }
 
-static JNINativeMethod methods[] = {
-        {"helloWorld", "()V",   (void *) zcy_native_helloWorld},
-        {"add",        "(II)I", (void *) zcy_native_add},
-        {"sayHello", "()Ljava/lang/String;", (void *) sayHello}
-};
+static JNINativeMethod methods[] = {{"helloWorld", "()V",                  (void *) zcy_native_helloWorld},
+                                    {"add",        "(II)I",                (void *) zcy_native_add},
+                                    {"sayHello",   "()Ljava/lang/String;", (void *) sayHello}};
 
-static int registNativeMethods(JNIEnv *env) {
+static int registNativeMethods(JNIEnv *env)
+{
     jclass clazz;
     LOGI("Registering %s native\n", classPathName);
     clazz = env->FindClass(classPathName);
-    if (clazz == NULL) {
+    if (clazz == NULL)
+    {
         LOGE("ERROR: Class not found");
         return JNI_FALSE;
     }
-    if ((env->RegisterNatives(clazz, methods, sizeof(methods) / sizeof(methods[0]))) < 0) {
+    if ((env->RegisterNatives(clazz, methods, sizeof(methods) / sizeof(methods[0]))) < 0)
+    {
         LOGE("Register natives failed for '%s'\n", classPathName);
         return JNI_ERR;
     }
@@ -60,11 +62,13 @@ static int registNativeMethods(JNIEnv *env) {
     return JNI_OK;
 }
 
-jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
+jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
+{
     JNIEnv *env = NULL;
     LOGI("OnLoad");
     mJVM = vm;
-    if (vm->GetEnv((void **) &env, JNI_VERSION_1_4) != JNI_OK) {
+    if (vm->GetEnv((void **) &env, JNI_VERSION_1_4) != JNI_OK)
+    {
         return JNI_EVERSION;
     }
     registNativeMethods(env);
