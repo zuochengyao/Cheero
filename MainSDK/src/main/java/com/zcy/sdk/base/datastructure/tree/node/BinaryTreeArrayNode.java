ad5a38aa9bdb5fd3fee959abf4a3e13b8cf9ecff
zuochengyao <zuochengyao@megvii.com>
2018-02-07 15:15:39 +0800

    datastructure

---
+++ MainSDK/src/main/java/com/zcy/sdk/base/datastructure/tree/node/BinaryTreeArrayNode.java
@@ -0,0 +1,41 @@
+package com.zcy.sdk.base.datastructure.tree.node;
+
+public class BinaryTreeArrayNode
+{
+	/**
+	 * 数组实现，保存的不是 左右子树的引用，而是数组下标
+	 */
+	private int mData;
+	private int mLeftChild;
+	private int mRightChild;
+
+	public int getData()
+	{
+		return mData;
+	}
+
+	public void setData(int data)
+	{
+		mData = data;
+	}
+
+	public int getLeftChild()
+	{
+		return mLeftChild;
+	}
+
+	public void setLeftChild(int leftChild)
+	{
+		mLeftChild = leftChild;
+	}
+
+	public int getRightChild()
+	{
+		return mRightChild;
+	}
+
+	public void setRightChild(int rightChild)
+	{
+		mRightChild = rightChild;
+	}
+}
