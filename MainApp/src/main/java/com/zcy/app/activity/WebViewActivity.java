386d07e77957f47ec36f85183155f1cb7d956b22
zuochengyao <zuochengyao@megvii.com>
2018-02-28 18:30:55 +0800

    webviewclient

--- MainApp/src/main/java/com/zcy/app/activity/WebViewActivity.java
+++ MainApp/src/main/java/com/zcy/app/activity/WebViewActivity.java
@@ -1,8 +1,14 @@
 package com.zcy.app.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class WebViewActivity extends Activity
 {
@@ -12,7 +18,34 @@ public class WebViewActivity extends Activity
         super.onCreate(savedInstanceState);
         WebView webView = new WebView(this);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://www.google.com/");
+        webView.setWebViewClient(mClient);
+        webView.loadUrl("https://www.baidu.com/");
         setContentView(webView);
     }
+
+    private WebViewClient mClient = new WebViewClient()
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
+        {
+            Uri uri = request.getUrl();
+            if (TextUtils.equals(uri.getAuthority(), "www.google.com"))
+                return false;
+            Toast.makeText(WebViewActivity.this, "Sorry, buddy!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon)
+        {
+            Uri uri = Uri.parse(url);
+            if (TextUtils.equals(uri.getAuthority(), "www.google.com"))
+                super.onPageStarted(view, url, favicon);
+            else
+            {
+                Toast.makeText(WebViewActivity.this, "Sorry, buddy!", Toast.LENGTH_SHORT).show();
+                view.stopLoading();
+            }
+        }
+    };
 }
