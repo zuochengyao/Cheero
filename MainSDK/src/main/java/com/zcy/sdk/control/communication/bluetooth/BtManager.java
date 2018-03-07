7ca2b21b78f2ed54c6436445c6d9b74c082bc939
zuochengyao <zuochengyao@megvii.com>
2018-03-07 14:29:23 +0800

    bluetooth unfinished

---
+++ MainSDK/src/main/java/com/zcy/sdk/control/communication/bluetooth/BtManager.java
@@ -0,0 +1,40 @@
+package com.zcy.sdk.control.communication.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+
+/**
+ * 蓝牙管理
+ * Created by zuochengyao on 2018/3/7.
+ */
+
+public class BtManager
+{
+    public static final Class<BtManager> TAG = BtManager.class;
+
+    private BluetoothAdapter mBtAdapter;
+    private BluetoothSocket mBtSocket;
+    private Context mContext;
+
+    private static volatile BtManager mInstance;
+
+    public static BtManager getInstance(Context context)
+    {
+        if (mInstance == null)
+        {
+            synchronized (BtManager.class)
+            {
+                if (mInstance == null)
+                    mInstance = new BtManager(context);
+            }
+        }
+        return mInstance;
+    }
+
+    private BtManager(Context context)
+    {
+        mContext = context.getApplicationContext();
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+}
