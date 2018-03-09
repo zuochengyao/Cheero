916ad158d527f543962ff1273d61212671a96200
zuochengyao <zuochengyao@megvii.com>
2018-03-09 11:14:04 +0800

    using PreferenceFragment

--- MainApp/src/main/java/com/zcy/app/activity/data/CustomSettingActivity.java
+++ MainApp/src/main/java/com/zcy/app/activity/data/CustomSettingActivity.java
@@ -1,17 +1,32 @@
 package com.zcy.app.activity.data;
 
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
 
 import com.zcy.app.R;
 
-public class CustomSettingActivity extends PreferenceActivity
+public class CustomSettingActivity extends Activity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.settings);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.add(android.R.id.content, new SettingFragment());
+        ft.commit();
+    }
+
+    public static class SettingFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+        }
     }
 }
