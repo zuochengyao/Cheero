306b8b072831d09cf02d3e380e79143c7df0f3db
zuochengyao <zuochengyao@megvii.com>
2018-03-16 14:38:44 +0800

    设计模式：备忘录

---
+++ MainSDK/src/main/java/com/zcy/sdk/basis/designpattern/memento/Originator.java
@@ -0,0 +1,40 @@
+package com.zcy.sdk.basis.designpattern.memento;
+
+import com.zcy.sdk.util.Log;
+
+/**
+ * 发起人：
+ * 负责创建一个备忘录Memento，用以记录当前时刻它的内部状态，并可使用备忘录回复内部状态
+ * 可根据需要决定Memento存储Originator的哪些内部状态
+ * Created by zuochengyao on 2018/3/16.
+ */
+
+public class Originator
+{
+    private String _state;
+
+    public String getState()
+    {
+        return _state;
+    }
+
+    public void setState(String _state)
+    {
+        this._state = _state;
+    }
+
+    public Memento createMemento()
+    {
+        return new Memento(_state);
+    }
+
+    public void setMemento(Memento memento)
+    {
+        _state = memento.getState();
+    }
+
+    public void show()
+    {
+        Log.i(Originator.class, "State = " + _state);
+    }
+}
