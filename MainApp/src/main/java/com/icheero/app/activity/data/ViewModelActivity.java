627ab64688138dc72d78fb5f49fe3415bb50889e
zuochengyao <zuochengyao@megvii.com>
2018-05-29 11:00:01 +0800

    mvvm test finish

--- MainApp/src/main/java/com/icheero/app/activity/data/ViewModelActivity.java
+++ MainApp/src/main/java/com/icheero/app/activity/data/ViewModelActivity.java
@@ -1,21 +1,19 @@
 package com.icheero.app.activity.data;
 
+import android.annotation.SuppressLint;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.icheero.app.R;
+import com.icheero.app.databinding.ActivityViewModelBinding;
 import com.icheero.app.model.User;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class ViewModelActivity extends AppCompatActivity
 {
+    /*
     @BindView(R.id.text_user_name)
     TextView tvUserName;
     @BindView(R.id.text_user_age)
@@ -24,28 +22,40 @@ public class ViewModelActivity extends AppCompatActivity
     TextView tvUserPhone;
     @BindView(R.id.button_user_update)
     Button btnUserUpdate;
-
+    */
     private User mUser;
-    private ViewDataBinding binding;
+    private ActivityViewModelBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_model);
-        ButterKnife.bind(this);
         mUser = new User();
         mUser.setAge(10);
         mUser.setName("Cheero");
         mUser.setPhoneNumber("18513141213");
+        binding.setUser(mUser);
+        mHandler.sendEmptyMessageDelayed(0, 3000);
     }
 
-    @OnClick(R.id.button_user_update)
-    void updateUser()
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler()
     {
-        mUser.setAge(11);
-        mUser.setName("Zero");
-        mUser.setPhoneNumber("18513141213=====");
-    }
-
+        @Override
+        public void handleMessage(Message msg)
+        {
+            super.handleMessage(msg);
+            switch (msg.what)
+            {
+                case 0:
+                {
+                    mUser.setAge(20);
+                    mUser.setName("Zero");
+                    binding.setUser(mUser);
+                    break;
+                }
+            }
+        }
+    };
 }
