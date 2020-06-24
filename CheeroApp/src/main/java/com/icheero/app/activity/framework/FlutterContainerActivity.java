dfd309769e9779780df32e06e40c5dc6e3b12a81
zcy_work <zuochengyao@megvii.com>
2020-06-24 18:35:26 +0800

    1. update flutter module
    2. BaseActivity 改动

--- CheeroApp/src/main/java/com/icheero/app/activity/framework/FlutterContainerActivity.java
+++ CheeroApp/src/main/java/com/icheero/app/activity/framework/FlutterContainerActivity.java
@@ -1,66 +1,90 @@
 package com.icheero.app.activity.framework;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.icheero.app.R;
-import com.icheero.sdk.base.BaseActivity;
+import com.icheero.sdk.util.Common;
 
-//import io.flutter.facade.Flutter;
-//import io.flutter.plugin.common.EventChannel;
-//import io.flutter.plugin.common.MethodChannel;
-//import io.flutter.view.FlutterView;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.view.FlutterView;
 
-public class FlutterContainerActivity extends BaseActivity
+public class FlutterContainerActivity extends FlutterActivity
 {
-//    FlutterView mFlutterView;
+    FlutterView mFlutterView;
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine)
+    {
+        super.configureFlutterEngine(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor(), "com.icheero.app.activity/fta").setMethodCallHandler((methodCall, result) -> {
+            if (!TextUtils.isEmpty(methodCall.method))
+            {
+                Common.toast(this, methodCall.method, Toast.LENGTH_SHORT);
+                result.success("success");
+            }
+            else
+                result.notImplemented();
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flutter_container);
+
         doInitFlutterView();
     }
 
     private void doInitFlutterView()
     {
-//        mFlutterView = Flutter.createView(this, getLifecycle(), "basicRoute");
-//        FrameLayout.LayoutParams layout = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
-//        addContentView(mFlutterView, layout);
-//        event();
-//        method();
+        mFlutterView = new FlutterView(this);
+        mFlutterView.setInitialRoute("basicRoute");
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        addContentView(mFlutterView, params);
+        event();
+        method();
     }
 
     private void method()
     {
-//        new MethodChannel(mFlutterView, "com.icheero.app.activity/fta").setMethodCallHandler((methodCall, result) -> {
-//            if (!TextUtils.isEmpty(methodCall.method))
-//            {
-//                Common.toast(this, methodCall.method, Toast.LENGTH_SHORT);
-//                result.success("success");
-//            }
-//            else
-//            {
-//                result.notImplemented();
-//            }
-//        });
+        new MethodChannel(mFlutterView, "com.icheero.app.activity/fta").setMethodCallHandler((methodCall, result) -> {
+            if (!TextUtils.isEmpty(methodCall.method))
+            {
+                Common.toast(this, methodCall.method, Toast.LENGTH_SHORT);
+                result.success("success");
+            }
+            else
+            {
+                result.notImplemented();
+            }
+        });
     }
 
     private void event()
     {
-//        new EventChannel(mFlutterView, "com.icheero.app.activity/atf").setStreamHandler(new EventChannel.StreamHandler()
-//        {
-//            @Override
-//            public void onListen(Object o, EventChannel.EventSink eventSink)
-//            {
-//                String param = "From Android Data";
-//                eventSink.success(param);
-//            }
-//
-//            @Override
-//            public void onCancel(Object o)
-//            {
-//            }
-//        });
+        new EventChannel(mFlutterView, "com.icheero.app.activity/atf").setStreamHandler(new EventChannel.StreamHandler()
+        {
+            @Override
+            public void onListen(Object o, EventChannel.EventSink eventSink)
+            {
+                String param = "From Android Data";
+                eventSink.success(param);
+            }
+
+            @Override
+            public void onCancel(Object o)
+            {
+            }
+        });
     }
 }
