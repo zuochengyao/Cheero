ae1f5d09a495ad8b4101949d3b0173fb4e947ae9
zcy_work <zuochengyao@megvii.com>
2021-05-11 18:49:02 +0800

    一个 bug

--- CheeroSDK/src/main/jni/Util/src/util.c
+++ CheeroSDK/src/main/jni/Util/src/util.c
@@ -21,19 +21,11 @@ void at_request_analysis(const char *request, int32 len, struct at_Request *atRe
         atReq = memMalloc(sizeof(at_Request));
         TRACE("LUAT", ANDROID_LOG_INFO, "atReq malloc success\n");
     }
-    int ret;
-    char buf1[255];
-    char buf2[255];
-    char buf3[255];
-    char buf4[255];
-    char buf5[255];
-    uint64 uid = 0;
-    char *pwd;
     // AT+POC=021234 000000025D30BDD5 613B5D2D293031622E
     // 000000025D30BDD5: 10153410005
     // 613B5D2D29303162: a;]-)01b
-    ret = sscanf(request, "%7s%2s%4s%llx", buf1, buf2, buf3, &uid);
+    sscanf(request, "%7s%2s%4s%llx", atReq->header, atReq->cmd, atReq->id, atReq->extra);
     TRACE("LUAT", ANDROID_LOG_INFO,
-          "[LUAT] 2.ret=%d, buf1=%s, buf2=%s, buf3=%s, uid=%llx\n", ret,
-          buf1, buf2, buf3, uid);
+          "[LUAT] 2.buf1=%s, buf2=%s, buf3=%s, uid=%llx\n", atReq->header, atReq->cmd, atReq->id,
+          atReq->extra);
 }
